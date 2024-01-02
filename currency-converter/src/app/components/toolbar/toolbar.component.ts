61784b4eefb62aa6a2e3258643680b2483a363d2
Adam Smith <>
2024-01-02 18:09:44 +0000

    refactor(converter): responsive design tweaks

--- currency-converter/src/app/components/toolbar/toolbar.component.ts
+++ currency-converter/src/app/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
@@ -11,9 +12,10 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule,
-    MatToolbarModule,
-    MatIconModule,
     MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
   ],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
